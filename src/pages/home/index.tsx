e169ab2d148acb0f63693d7cf1261d654539f2cb
adrisad <22082002andrademedina@email.com>
2025-05-08 17:39:21 -0400

    feat: configurando el enrutamiento

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+function index() {
+  return (
+    <>
+        <h1 className="bg-amber-500">hola soy el home</h1>
+    </>
+  );
+}
+
+export default index
---
+++ src/routers/routes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Home from "../pages/home"
+function routes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default routes
